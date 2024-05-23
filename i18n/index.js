ea620e4d8e14d00a9286b319fefef4ecdf9dcc51
Kaoré <kaore@openagenda.com>
2024-05-23 08:38:07 +0200

    added italian

--- i18n/index.js
+++ i18n/index.js
@@ -3,9 +3,11 @@
 const fr = require('./fr.json');
 const en = require('./en.json');
 const de = require('./de.json');
+const it = require('./it.json');
 
 module.exports = {
   fr,
   en,
   de,
+  it,
 };
